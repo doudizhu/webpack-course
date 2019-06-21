bc07edcdb20c5dcf42669c3cf4143e67f35a10e6
lipeng <lipeng@daokoudai.com>
2019-06-21 15:57:50 +0800

    5.webpack4-polyfill和transform和preset

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,13 @@ const path = require('path')
 module.exports = {
   // 入口：有并且可以有多个
   entry: {
+    // main: [
+    //   'babel-polyfill','./src/main.js'
+    // ],
+    // main: [
+    //   'core-js/fn/promise',
+    //   './src/main.js'
+    // ],
     main: [
       './src/main.js'
     ],
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // alert('Hello World!')
 
+require('@babel/runtime/regenerator')
 require('./main.css')
 require('./index.html')
 
-var a = () => {
-  console.log('Hello Future!')
+var a = async args => {
+  const {a, b} = args
+  await console.log('Hello Future!', a, b)
+  console.log('Done')
 }
+
+a({a:1,b:2})
